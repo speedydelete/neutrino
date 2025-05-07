5d2da710a40263ce5641e16b4eb7adfd916109db
speedydelete <speedykorok777@gmail.com>
2025-05-07 19:10:27 -0400

    delete src_old

--- src_old/imports.ts
+++
@@ -1,7 +0,0 @@
-
-import {Type} from './types';
-
-
-export function getImportType(path: string, name: string | Type, options?: Type): Type {
-    throw new Error('Imports are not supported');
-}
--- src_old/index.ts
+++
@@ -1,8 +0,0 @@
-
-export * from './highlighter';
-export * as t from './types';
-export {Type} from './types';
-export * from './util';
-export {Inferrer} from './inferrer';
-export {Generator} from './generator';
-export {compile} from './compiler';
