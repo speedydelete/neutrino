98b0e17e989ca75c235dab58e4727ff169e4aa4e
speedydelete <speedykorok777@gmail.com>
2025-05-09 18:42:12 -0400

    fix another bug

--- builtins/core/util.h
+++ builtins/core/util.h
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <math.h>
-#include </home/speedy/code/windows-libgc/include/gc.h>
+#include <gc.h>
 
 #define JS_NULL (void**)NULL
 #define NaN (double)NAN
